1ea6c277c0280b8f88b8a7e78760d4e20c94d800
Abderraouf_Kdr <Kdr.med.abderraouf@gmail.com>
2025-12-28 21:15:40 +0100

    Add Client model

--- is_transport/core/models.py
+++ is_transport/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    nom = models.CharField(max_length=100, null=False)
+    prenom = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telephone = models.CharField(max_length=20)
+    adresse = models.TextField()
+    solde = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.nom} {self.prenom}"
