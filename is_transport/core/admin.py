f4e23d03a3370364ed43355d4cf2b0e1c99e66b4
Abderraouf_Kdr <Kdr.med.abderraouf@gmail.com>
2025-12-29 16:57:51 +0100

    migrations

--- is_transport/core/admin.py
+++ is_transport/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Client,Reclamation
+from .models import Client,Reclamation,Chauffeur
 admin.site.register(Client)
 admin.site.register(Reclamation)
+admin.site.register(Chauffeur)
