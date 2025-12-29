de618da0649b542b72079bc11f864f7d235b26dc
Abderraouf_Kdr <Kdr.med.abderraouf@gmail.com>
2025-12-29 14:09:53 +0100

    message

--- is_transport/core/admin.py
+++ is_transport/core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 
-from .models import Client
+from .models import Client,Reclamation
 admin.site.register(Client)
+admin.site.register(Reclamation)
--- is_transport/core/models.py
+++ is_transport/core/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Client(models.Model):
-    nom = models.CharField(max_length=100, null=False)
+    id_client = models.CharField(max_length=20, unique=True)
+    nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     telephone = models.CharField(max_length=20)
@@ -10,4 +11,14 @@ class Client(models.Model):
     solde = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
-        return f"{self.nom} {self.prenom}"
+      return f"{self.nom} {self.prenom}"
+
+class Reclamation(models.Model):
+    id_reclamation = models.CharField(max_length=20, unique=True)
+    nature_reclamation = models.CharField(max_length=150)
+    date_reclamation = models.DateField(auto_now_add=True)
+    etat_reclamation =models.CharField(max_length=50, default='En cours')
+    id_client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='reclamations')
+                                
+    def __str__(self):
+      return self.id_reclamation
